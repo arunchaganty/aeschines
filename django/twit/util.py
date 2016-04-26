03742691885123cedc581363143848fa2149ed6c
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2016-04-25 22:50:01 -0700

    Fixed twitter streaming

--- django/twit/util.py
+++ django/twit/util.py
@@ -6,6 +6,7 @@ Various utilities for interfacing with Twitter
 from twitter import Twitter, OAuth, TwitterStream
 from twitter.api import TwitterHTTPError
 from twit.settings import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET, STREAM_CONSUMER_KEY, STREAM_CONSUMER_SECRET, STREAM_ACCESS_KEY, STREAM_ACCESS_SECRET
+import ipdb
 
 SEARCH_TEMPLATE = '{kw} -RT -filter:media -filter:links'
 RATE_LIMIT = 180
@@ -58,11 +59,18 @@ def slurp_tweets(api, keyword, max_id = -1):
         max_id = lst[-1]['id'] - 1
 
 def tweet_filter(tweet):
-    if tweet['lang'] != 'en': return False
-    if 'media' in tweet['entities'] and len(tweet['entities']['media']) > 0: return False
-    if 'urls' in tweet['entities'] and len(tweet['entities']['urls']) > 0: return False
-
-    return True
+    """
+    Filter tweets to only be:
+        - English
+        - Contain no media
+    """
+    try:
+        if tweet['lang'] != 'en': return False
+        if 'media' in tweet['entities'] and len(tweet['entities']['media']) > 0: return False
+        #if 'urls' in tweet['entities'] and len(tweet['entities']['urls']) > 0: return False
+        return True
+    except KeyError:
+        return False
 
 def stream_tweets(api, keywords):
     """
