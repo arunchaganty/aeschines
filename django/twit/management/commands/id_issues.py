1c83f01e4d0262e61be7755a16dd28720a1765cd
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2016-05-18 13:30:34 -0700

    Added user_id to output

--- django/twit/management/commands/id_issues.py
+++ django/twit/management/commands/id_issues.py
@@ -17,28 +17,33 @@ class Command(BaseCommand):
         parser.add_argument('--output', default=sys.stdout, type=argparse.FileType('w'), help="Where the output is written to.")
         parser.add_argument('--write-output', action='store_true', default=False, help="Will write [id,text,issues] to output if set.")
 
-    def handle(self, *args, **options):
-        if options['input'] is not None:
-            with open(options['input']) as f:
-                reader = csv.reader(f, delimiter='\t')
-                header = next(reader)
-                assert header == ["id", "text"]
-
-                tweets = list(reader)
-        else: 
-            tweets = map(lambda t: (t.id, t.text), Tweet.objects.all())
-        writer = csv.writer(options['output'], delimiter="\t") if options['write_output'] else None
+    def build_counts(self, tweets, output=None): 
+        writer = csv.writer(output, delimiter="\t") if output is not None else None
         if writer is not None:
-            writer.writerow(['id', 'text', 'issues'])
+            writer.writerow(['id', 'user_id', 'text', 'issues'])
 
         counts = Counter()
         print("Building counts by issue")
-        for id, text in tweets:
+        for id, user_id, text in tweets:
             issues = identify_issues(text.lower())
             if writer is not None:
-                writer.writerow([id, text, ",".join(issues.keys())])
+                writer.writerow([id, user_id, text, ",".join(issues.keys())])
             counts.update(issues)
 
         print("Issues referenced most often in descending order")
         for issue, count in counts.most_common():
             print("%4d %s" % (count, issue))
+
+    def handle(self, *args, **options):
+        if options['input'] is not None:
+            with open(options['input']) as f:
+                reader = csv.reader(f, delimiter='\t')
+                header = next(reader)
+                assert header == ["id", "user_id", "text"]
+
+                tweets = reader # Generator!
+                self.build_counts(tweets, options['output'] if options['write_output'] is not None else None)
+        else:
+            tweets = map(lambda t: (t.id, t.user_id, t.text), Tweet.objects.all())
+            self.build_counts(tweets, options['output'] if options['write_output'] is not None else None)
+
