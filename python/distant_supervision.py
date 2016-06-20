31d5f0ec17c6fa79277f7fcd29b969e95fb13101
Arun Tejasvi Chaganty <arunchaganty@gmail.com>
2016-06-20 13:54:53 -0700

    Updated the distant supervision function.

--- python/distant_supervision.py
+++ python/distant_supervision.py
@@ -9,31 +9,98 @@ http://arxiv.org/abs/1605.05150.
 """
 from __future__ import print_function
 
-import sys
 import csv
 
 import numpy as np
 from keras import backend as K
 from keras.models import Sequential
+from keras.models import model_from_json
 from keras.layers.core import Dense, Dropout, Activation, Reshape
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 
-def do_command(args):
-    tweets = RowObjectFactory.from_stream(csv.reader(args.input))
-    for row in reader:
-        writer.writerow(row)
+def load_data(npz, dev_split):
+    """
+    Load training data from numpy.
+    """
+    data = np.load(npz)
+    X_train, y_train = data['X_train'], data['y_train']
+    n_train, n_val = len(X_train) - int(len(X_train) * dev_split), int(len(X_train) * dev_split)
+    X_train, X_val = X_train[:n_train], X_train[n_train:]
+    y_train, y_val = y_train[:n_train], y_train[n_train:]
+
+    return X_train, y_train, X_val, y_val
+
+def save_model(model, model_output, weights_output):
+    """
+    Save model architecture and weights.
+    """
+    model_output.write(model.to_json())
+    model.save_weights(weights_output)
+
+def load_model(model, model_input, weights_input):
+    """
+    Load model architecture and weights.
+    """
+    model = model_from_json(model_input)
+    model.load_weights(weights_input)
+    return model
+
+def build_model(args):
+    model = Sequential()
+
+    # Input layer.
+    # data.
+
+    # 1d convolution
+
+    return model
+
+def do_train(args):
+    """
+    Train the model using the provided arguments.
+    """
+    # Build model
+    model = build_model(args)
+
+    # Load data.
+    X_train, y_train, X_val, y_val = load_data(args.input, args.dev_split)
+
+    # Train the model.
+    for _ in range(args.training_epochs):
+        print("=== Training the model.")
+        model.fit(X_train, y_train, nb_epoch=5, batch_size=32)
+
+        print("=== validiation error")
+        model.evaluate(X_val, y_val, batch_size=32)
+
+    # Save the model
+    save_model(model, args.model, args.weights)
+
+def do_run(args):
+    """
+    Run the neural net to predict on new data.
+    """
+    # Load the model and weights
+    # For each input example, predict the output
+    raise NotImplementedError()
 
 if __name__ == "__main__":
-    import sys
     import argparse
-    parser = argparse.ArgumentParser(description='')
-    parser.add_argument('--input', type=argparse.FileType('r'), default=sys.stdin, help="Input file")
-    parser.add_argument('--output', type=argparse.FileType('w'), default=sys.stdout, help="Output file")
-    parser.set_defaults(func=do_command)
+    parser = argparse.ArgumentParser(description='Distant supervision')
 
-    #subparsers = parser.add_subparsers()
-    #command_parser = subparsers.add_parser('command', help='' )
-    #command_parser.set_defaults(func=do_command)
+    subparsers = parser.add_subparsers()
+    command_parser = subparsers.add_parser('train', help='Trains the distant supervision model')
+    command_parser.add_argument('--input', type=argparse.FileType('r'), help="Input tweets represented as a 150x70 matrix (each)")
+    command_parser.add_argument('--model', type=argparse.FileType('w'), help="Where to save the model")
+    command_parser.add_argument('--dev_split', type=float, default=0.1, help="How much of the data to save for dev.")
+    command_parser.add_argument('--training_epochs', type=int, default=10, help="Number of epochs (full passes of the data) to train the model on.")
+    command_parser.add_argument('--weights', type=str, help="Where to save the model weights")
+    command_parser.set_defaults(func=do_train)
+
+    command_parser = subparsers.add_parser('run', help='Runs the distant supervision model on individual models')
+    command_parser.add_argument('--input', type=argparse.FileType('r'), default=sys.stdin, help="Input tweets")
+    command_parser.add_argument('--output', type=argparse.FileType('w'), default=sys.stdout, help="Output labels on the tweets")
+    command_parser.set_defaults(func=do_run)
 
 
     ARGS = parser.parse_args()
--- python/prepare_data.py
+++ python/prepare_data.py
@@ -5,7 +5,9 @@
 
 import csv
 import numpy as np
-from util import Index, to_ascii, ordc
+from scipy.sparse import csc_matrix
+from util import Index, to_ascii, ordc, RowObjectFactory
+from tqdm import tqdm
 
 def to_features(text):
     """
@@ -21,16 +23,16 @@ def to_features(text):
 
     The encoding of characters corresponds to the ASCII table from 32 (' ') to 126 ('~').
     """
-    assert len(text) <= 150
-    features = np.zeros((150, 96))
-    for i, c in enumerate(text):
-        features[i][idx(c)] = 1
-    return features
+    text = text[:150]
+    assert len(text) <= 150, "Tweet is too long."
 
-def int_(lbl):
-    return int(lbl) if lbl != '' else 0
+    row, col, data = zip(*((i, ordc(c), 1) for i, c in enumerate(text)))
+    return csc_matrix((data, (row, col)), shape=(150, 96))
 
-def prepare_data(istream):
+def float_(lbl):
+    return float(lbl) if lbl != '' else 0
+
+def prepare_data(tweets):
     """
     Assumes that the input is a stream of a Tweet objects that contain
     the attributes:
@@ -38,33 +40,20 @@ def prepare_data(istream):
         - label
     Labels are also returned as one-hot vectors.
     """
-    #n_instances, labels = get_data_spec(istream)
-    tweets = RowObjectFactory.from_stream(csv.reader(istream))
-    #X_train, y_train = np.zeros((n_instances,150, 96)), np.zeros((n_instances, len(labels)))
     X_train, y_train = [], []
 
-    for i, tweet in enumerate(tweets): 
+    for tweet in tqdm(tweets):
         feats = to_features(to_ascii(tweet.text))
-        labels = [int_(tweet.hc), int_(tweet.bs), int_(tweet.dt), int_(tweet.tc)]
+        labels = [float_(tweet.hc), float_(tweet.bs), float_(tweet.dt), float_(tweet.tc)]
 
         X_train.append(feats)
         y_train.append(labels)
 
     return np.array(X_train), np.array(y_train)
 
-#def get_data_spec(istream):
-#    """
-#    Read input to get some properties from it. Resets the stream to the beginning when done.
-#    """
-#    n_instances, labels = 0, Index()
-#    for obj in RowObjectFactory.from_stream(csv.reader(istream)):
-#        n_instances += 1
-#        labels[obj.label]
-#    istream.seek(0)
-#    return n_instances, labels
-
 def do_command(args):
-    X_train, y_train = prepare_data(args.input)
+    tweets = RowObjectFactory.from_stream(csv.reader(args.input, delimiter="\t"))
+    X_train, y_train = prepare_data(tweets)
     np.savez(args.output, X_train = X_train, y_train = y_train)
 
 if __name__ == "__main__":
@@ -72,13 +61,8 @@ if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Process a file with labels')
     parser.add_argument('--input', type=argparse.FileType('r'), default=sys.stdin, help="Input file")
-    parser.add_argument('--output', type=argparse.FileType('w'), default=sys.stdout, help="Output file")
+    parser.add_argument('--output', type=argparse.FileType('wb'), default=sys.stdout, help="Output file")
     parser.set_defaults(func=do_command)
-
-    #subparsers = parser.add_subparsers()
-    #command_parser = subparsers.add_parser('command', help='' )
-    #command_parser.set_defaults(func=do_command)
-
 
     ARGS = parser.parse_args()
     ARGS.func(ARGS)
