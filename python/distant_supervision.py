8387d99507f98e9e0ff99d899e7a318ea0b47d95
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2016-06-21 11:48:41 -0700

    Have a silly model predicting stance

--- python/distant_supervision.py
+++ python/distant_supervision.py
@@ -11,22 +11,42 @@ from __future__ import print_function
 
 import csv
 
+import ipdb
+
 import numpy as np
+from numpy import zeros, array
 from keras import backend as K
 from keras.models import Sequential
 from keras.models import model_from_json
-from keras.layers.core import Dense, Dropout, Activation, Reshape
+from keras.layers.core import Dense, Dropout, Activation, Reshape, Flatten
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
 
-def load_data(npz, dev_split):
+from util import tokenize, Index, to_ascii, ordc, RowObjectFactory, log, WordVectorModel
+
+def float_(lbl):
+    return float(lbl) if lbl != '' else 0.
+
+def process_input(tweet):
+    tokens = tokenize(to_ascii(tweet.text))
+    labels = [float_(tweet.hc), float_(tweet.bs), float_(tweet.dt), float_(tweet.tc)]
+    return tokens, labels
+
+def load_data_raw(istream, dev_split):
     """
-    Load training data from numpy.
+    Load training data from a stream of input.
+    Input is a TSV with fields (id, text, label).
+    Returns a set of words, and input.
     """
-    data = np.load(npz)
-    X_train, y_train = data['X_train'], data['y_train']
-    n_train, n_val = len(X_train) - int(len(X_train) * dev_split), int(len(X_train) * dev_split)
-    X_train, X_val = X_train[:n_train], X_train[n_train:]
-    y_train, y_val = y_train[:n_train], y_train[n_train:]
+    log("Loading training data...")
+    data = list(map(process_input, RowObjectFactory.from_stream(csv.reader(istream, delimiter="\t"))))
+    np.random.shuffle(data)
+
+    # Make splits of train and validation data.
+    n_train, n_val = len(data) - int(len(data) * dev_split), int(len(data) * dev_split)
+    X_train, y_train = zip(*data[:n_train])
+    X_val, y_val = zip(*data[n_train:])
+
+    log("Done. Loaded {} train {} val instances", n_train, n_val)
 
     return X_train, y_train, X_val, y_val
 
@@ -45,13 +65,30 @@ def load_model(model, model_input, weights_input):
     model.load_weights(weights_input)
     return model
 
-def build_model(args):
+def build_model(args, input_shape):
     model = Sequential()
 
-    # Input layer.
-    # data.
+    N, D = input_shape
 
-    # 1d convolution
+    # 2D Convolution - 200 filters, with a filter size of LxD
+
+    # Max pooling layer
+
+    # droput
+    # Dense to k = 200
+    # dropout
+    # Softmax to 4.
+
+    # Input layer.
+    model.add(Reshape((N*D,), input_shape=input_shape))
+    model.add(Dense(
+        4,
+        activation='linear'))
+
+    model.compile(
+        optimizer='rmsprop',
+        loss='mse',
+        metrics=['accuracy'])
 
     return model
 
@@ -59,22 +96,28 @@ def do_train(args):
     """
     Train the model using the provided arguments.
     """
+
+    X_train, y_train, X_val, y_val = load_data_raw(args.input, args.dev_split)
+    wvecs = WordVectorModel.from_file(args.wvecs, False, '*UNKNOWN*')
+
+    N, D = args.n_words, wvecs.dim
+
     # Build model
-    model = build_model(args)
+    model = build_model(args, input_shape=(N,D))
 
     # Load data.
-    X_train, y_train, X_val, y_val = load_data(args.input, args.dev_split)
+    X_train, y_train, X_val, y_val = wvecs.embed_sentences(X_train), array(y_train), wvecs.embed_sentences(X_val), array(y_val)
 
     # Train the model.
     for _ in range(args.training_epochs):
-        print("=== Training the model.")
+        log("=== Training the model.")
         model.fit(X_train, y_train, nb_epoch=5, batch_size=32)
 
-        print("=== validiation error")
-        model.evaluate(X_val, y_val, batch_size=32)
+        score = model.evaluate(X_val, y_val, batch_size=32)
+        log("=== validiation error: loss: {}, acc: {}", *score)
 
-    # Save the model
-    save_model(model, args.model, args.weights)
+    ## Save the model
+    #save_model(model, args.model, args.weights)
 
 def do_run(args):
     """
@@ -85,16 +128,19 @@ def do_run(args):
     raise NotImplementedError()
 
 if __name__ == "__main__":
-    import argparse
+    import sys, argparse
     parser = argparse.ArgumentParser(description='Distant supervision')
+    parser.add_argument('--seed', default=42, help="Random seed to use")
 
     subparsers = parser.add_subparsers()
     command_parser = subparsers.add_parser('train', help='Trains the distant supervision model')
-    command_parser.add_argument('--input', type=argparse.FileType('r'), help="Input tweets represented as a 150x70 matrix (each)")
+    command_parser.add_argument('--input', type=argparse.FileType('r'), help="Input tweets represented as a TSV with fields id, text, label")
+    command_parser.add_argument('--wvecs', type=argparse.FileType('r'), help="Input word vector embeddings")
     command_parser.add_argument('--model', type=argparse.FileType('w'), help="Where to save the model")
     command_parser.add_argument('--dev_split', type=float, default=0.1, help="How much of the data to save for dev.")
     command_parser.add_argument('--training_epochs', type=int, default=10, help="Number of epochs (full passes of the data) to train the model on.")
     command_parser.add_argument('--weights', type=str, help="Where to save the model weights")
+    command_parser.add_argument('--n_words', type=int, default=50, help="Number of words to use.")
     command_parser.set_defaults(func=do_train)
 
     command_parser = subparsers.add_parser('run', help='Runs the distant supervision model on individual models')
@@ -102,7 +148,9 @@ if __name__ == "__main__":
     command_parser.add_argument('--output', type=argparse.FileType('w'), default=sys.stdout, help="Output labels on the tweets")
     command_parser.set_defaults(func=do_run)
 
-
     ARGS = parser.parse_args()
+
+    np.random.seed(ARGS.seed)
+
     ARGS.func(ARGS)
 
--- python/util.py
+++ python/util.py
@@ -4,7 +4,9 @@
 Useful utilities
 """
 
+import sys
 import numpy as np
+from numpy import array, zeros
 import csv
 from happyfuntokenizing import Tokenizer
 TOKENIZER = Tokenizer(preserve_case=True)
@@ -12,6 +14,12 @@ TOKENIZER = Tokenizer(preserve_case=True)
 from happyfuntokenizing import Tokenizer
 TOKENIZER = Tokenizer(preserve_case=True)
 
+
+BLUE = '\033[34;1m'
+CLOSE = '\033[0m'
+
+def log(msg, *args):
+    sys.stderr.write(BLUE + str(msg).format(*args) + CLOSE + "\n")
 
 class Index(object):
     """
@@ -61,8 +69,13 @@ def to_ascii(text):
     for asci, unicod in UNICODE_EMOJI_TO_ASCII_TABLE:
         text = text.replace(unicod, asci)
     text = text.encode('ascii', errors='ignore').decode('ascii', errors='ignore')
-    text = " ".join(TOKENIZER.tokenize(text))
     return text
+
+def tokenize(text):
+    """
+    Call the tokenizer
+    """
+    return TOKENIZER.tokenize(text)
 
 def ordc(c):
     """
@@ -109,4 +122,88 @@ def make_one_hot(vec, n_classes):
     for i, j in enumerate(vec):
         mat[i,j] = 1
     return mat
+
+class WordVectorModel(dict):
+    """
+    A wrapper around a word vector model.
+    """
+
+    def __init__(self, wvecs, dim, preserve_case=False, unknown='*UNKNOWN*'):
+        dict.__init__(self, wvecs)
+        self.dim = dim
+        self.preserve_case = preserve_case
+        self.unknown = unknown
+
+    def __getitem__(self, key):
+        if not self.preserve_case:
+            key = str.lower(key)
+        try:
+            return dict.__getitem__(self, key)
+        except KeyError:
+            return dict.__getitem__(self, self.unknown)
+
+    def __setitem__(self, key, val):
+        if not self.preserve_case:
+            key = str.lower(key)
+        return dict.__setitem__(self, key, val)
+
+    @staticmethod
+    def from_file(f, preserve_case, unknown):
+        """
+        Construct a word vector map from a file
+        """
+        log("Reading word vectors")
+        wvecs = {}
+        dim = None
+        for line in f:
+            parts = line.split()
+            tok = parts[0]
+            vec = array([float(x) for x in parts[1:]])
+            if dim is None:
+                dim = len(vec)
+            assert dim == len(vec), "Incorrectly sized vector"
+            wvecs[tok] = vec
+        assert unknown in wvecs, "Unknown token not defined in word vectors"
+        log("Done. Loaded {} vectors.", len(wvecs))
+        return WordVectorModel(wvecs, dim, preserve_case, unknown)
+
+    @staticmethod
+    def from_filename(fname, preserve_case, unknown):
+        """
+        Construct a word vector map from a file
+        """
+        with open(fname) as f:
+            return WordVectorModel.from_file(f, preserve_case, unknown)
+
+    def embed_sentence(self, toks, max_length=50):
+        """
+        Return the list of tokens embedded as a matrix.
+        """
+        X = zeros((max_length, self.dim))
+        for i, w in enumerate(toks[:max_length]):
+            X[i, :] = self[w]
+        return X
+
+    def embed_sentences(self, sentences, max_length=50):
+        """
+        Return the list of tokens embedded as a matrix.
+        """
+        return array([self.embed_sentence(toks, max_length) for toks in sentences])
+
+
+def test_wvec_model():
+    """
+    Test that the WordVectorModel handles case and unknowns correctly.
+    """
+    import os
+    from numpy import allclose
+    assert os.path.exists('./glove.6B.50d.txt'), "Can't find word vector file at"
+    undiplomatically = array([-0.44594,-1.1723,0.058833,-0.99806,0.065609,0.059089,0.60732,1.2965,-0.32984,-0.045185,-0.30061,0.33055,-0.18897,0.82746,-0.28756,-0.31784,-0.091562,-0.42301,1.173,-0.65538,0.027537,0.1238,0.26689,0.39363,0.62385,0.847,1.0387,0.82124,-0.064256,0.043767,-0.97034,-0.4336,1.1662,0.24741,0.54262,0.58686,0.51069,0.67763,-0.78139,-0.21806,0.029529,0.11175,-0.43608,0.41791,-0.34094,-1.0393,0.64999,0.2285,-0.033636,0.23816])
+    unk = array([-0.1292,-0.2887,-0.01225,-0.05677,-0.2021,-0.08389,0.3336,0.1605,0.03867,0.1783,0.04697,-0.002858,0.291,0.04614,-0.2092,-0.06613,-0.06822,0.07666,0.3134,0.1785,-0.1226,-0.09917,-0.07496,0.06413,0.1444,0.6089,0.1746,0.05335,-0.01274,0.03474,-0.8124,-0.04689,0.2019,0.2031,-0.03936,0.06968,-0.01554,-0.03405,-0.06528,0.1225,0.1399,-0.1745,-0.08012,0.08495,-0.01042,-0.137,0.2013,0.1007,0.00653,0.01685])
+
+    model = WordVectorModel.from_file('./glove.6B.50d.txt', False, '*UNKNOWN*')
+    assert allclose(undiplomatically, model['undiplomatically']), "Vectors are not equal!"
+    assert allclose(undiplomatically, model['UndIplomAtically']), "Vectors are not equal!"
+    assert allclose(unk, model['undiplomaticallyy']), "Vectors are not equal!"
+
 
