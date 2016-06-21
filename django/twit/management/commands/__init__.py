2edb83b45ac8be7a4bd7ef401abdd9d1de23d113
Arun Tejasvi Chaganty <chaganty@stanford.edu>
2016-06-21 10:21:09 -0700

    Added support for tqdm

--- django/twit/management/commands/__init__.py
+++ django/twit/management/commands/__init__.py
@@ -21,16 +21,28 @@ class DataProcessingCommand(BaseCommand):
         """Must define the schema of the output"""
         raise NotImplementedError()
 
-    def process(self, istream):
+    def process(self, istream, count=None):
         """
         Client must return ostream
         """
         raise NotImplementedError()
 
     def db_to_istream(self):
+        """
+        Creates a stream of objects from the db.
+        """
         raise NotImplementedError("Must define fn to create input stream from database")
 
+    def db_istream_count(self):
+        """
+        Count of elements in istream.
+        """
+        raise NotImplementedError("Must define fn to compute size of stream")
+
     def ostream_to_db(self, ostream):
+        """
+        Writes a stream of objects to the db.
+        """
         raise NotImplementedError("Must define fn to load database from output stream")
 
     def file_to_istream(self, ifstream):
@@ -41,6 +53,12 @@ class DataProcessingCommand(BaseCommand):
         header = next(reader)
         factory = RowObjectFactory(header)
         return map(factory.build, reader)
+
+    def file_istream_count(self, ifstream):
+        """
+        Returns count of number of elements in the line
+        """
+        return len(ifstream)
 
     def ostream_to_file(self, ofstream, ostream):
         """
@@ -61,8 +79,18 @@ class DataProcessingCommand(BaseCommand):
         else:
             istream = self.db_to_istream()
 
+        if options['input'] is not None:
+            if options['input'] == "-":
+                count = None
+            else:
+                ifstream = open(options['input'], 'r')
+                count = self.file_istream_count(ifstream)
+        else:
+            count = self.db_istream_count()
+            istream = self.db_to_istream()
+
         # Actually process the data.
-        ostream = self.process(istream)
+        ostream = self.process(istream, count)
 
         if options['output'] is not None:
             if options['output'] == "-":
--- django/twit/management/commands/annotate_stance.py
+++ django/twit/management/commands/annotate_stance.py
@@ -1,7 +1,8 @@
 """Annotate tweets with stance markers"""
 
 from twit.models import Tweet
-from twit.util import identify_stance
+from twit.util import identify_stance, queryset_iterator
+from twit.tqdm import tqdm
 
 from . import DataProcessingCommand
 
@@ -9,20 +10,26 @@ class Command(DataProcessingCommand):
     """Annotate tweets with stance markers"""
     help = __doc__
 
-    def process(self, istream):
+    def process(self, istream, count=None):
         """
         Each element of istream is a tweet.
         Use the text as a marker.
         Return (id, text, score).
         Client must return (header, ostream)
         """
+
+        if count is not None:
+            istream = tqdm(istream, total=count)
         for tweet in istream:
             stance = identify_stance(tweet.text.lower())
             for candidate, value in stance.items():
                 yield [tweet.id, candidate, value, 1.0]
 
     def db_to_istream(self):
-        return Tweet.objects.all()
+        return queryset_iterator(Tweet.objects.all())
+
+    def db_istream_count(self):
+        return Tweet.objects.count()
 
     def output_header(self):
         return ["tweet_id", "entity", "stance", "score"]
